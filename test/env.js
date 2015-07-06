9213572304c389848ebcdca19c4ab0088da6b96f
Alexander Pirsig <self@pirsig.net>
2015-07-06 11:56:01 +0200

    use const + template strings

--- test/env.js
+++ test/env.js
@@ -13,10 +13,12 @@ describe('Environment test', function(){
 
 describe('loading JSON from server', function(){
 
+  const urlBasePath = 'http://katas.tddbin.com/katas/es6/language';
+
   it('file exists and structure has `groups`', function(done){
 
     // arrange
-    var validUrl = 'http://katas.tddbin.com/katas/es6/language/__grouped__.json';
+    const validUrl = `${urlBasePath}/__grouped__.json`;
 
 
     // act
@@ -24,7 +26,6 @@ describe('loading JSON from server', function(){
 
       // assert
       assert.equal(err, null);
-      //assert.equal(result.substr(0,13),"{\n  \"groups\":");
       assert.equal('groups' in result, true);
       done();
 
@@ -35,7 +36,7 @@ describe('loading JSON from server', function(){
   it('file exists and structure has NO `groups`', function(done){
 
     // arrange
-    var validUrl = 'http://katas.tddbin.com/katas/es6/language/__all__.json';
+    const validUrl = `${urlBasePath}/__all__.json`;
 
 
     // act
@@ -43,7 +44,6 @@ describe('loading JSON from server', function(){
 
       // assert
       assert.equal(err, null);
-      //assert.equal(result.substr(0,13),"{\n  \"groups\":");
       assert.deepEqual(result, {});
       done();
 
