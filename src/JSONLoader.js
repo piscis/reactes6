9024a2c679a6bfaf8e15ec2bfa3d44b5a1035e1c
Alexander Pirsig <self@pirsig.net>
2015-07-06 13:27:19 +0200

    export default

--- src/JSONLoader.js
+++ src/JSONLoader.js
@@ -1,26 +1,35 @@
 import http from 'http';
 import url from 'url';
 
-export function loadRemoteFile(fileUrl, onLoaded) {
 
-  var data = '';
-  var options = url.parse(fileUrl);
-  var request = http.request(options, function(res) {
+export default class JSONLoader {
 
-    res.on('data', function(chunk) {data += chunk;});
-    res.on('end', function() {
-      var result = JSON.parse(data);
+  static loadRemoteFile(fileUrl, onLoaded) {
 
-      if(("groups" in result)){
-        onLoaded(null, result);
-      }else{
-        onLoaded(null, {});
-      }
+    var data = '';
+    var options = url.parse(fileUrl);
+    var request = http.request(options, function (res) {
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function () {
+        var result = JSON.parse(data);
+
+        if (("groups" in result)) {
+          onLoaded(null, result);
+        } else {
+          onLoaded(null, {});
+        }
+      });
 
     });
 
-  });
+    request.on('error', function (e) {
+      onLoaded(e);
+    });
 
-  request.on('error', function(e) { onLoaded(e); });
-  request.end();
+    request.end();
+  }
 };
