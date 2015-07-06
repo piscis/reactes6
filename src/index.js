5c486d2c2a1b6fec63afe7dc6ee3cd73665e5ae1
Alexander Pirsig <self@pirsig.net>
2015-07-06 14:38:49 +0200

    fix dist include

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ class Page extends React.Component {
     return (
       <ul>
         <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
       </ul>
     )
   }
